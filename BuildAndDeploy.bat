730dfbcac4d203ad46b824b40faa638222accbc8
bpirk <bastian.pirk@iav.de>
2020-03-28 10:14:17 +0100

    wip

--- BuildAndDeploy.bat
+++
@@ -1,5 +0,0 @@
-
-del /s /q D:\projects\corona-charts\charts\Docker\dist\*
-rmdir /s /q D:\projects\corona-charts\charts\Docker\dist
-mkdir /s /q D:\projects\corona-charts\charts\Docker\dist
-xcopy /s D:\projects\corona-charts\charts\dist D:\projects\corona-charts\charts\Docker\dist
